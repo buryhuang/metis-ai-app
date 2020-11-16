68056236fefe299e8f7682a76beb870be61a5a64
Bury Huang <bury.huang@highspot.com>
2020-11-15 21:04:11 -0800

    add basic icons and menus

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 import Navbar from 'react-bootstrap/Navbar'
 import {Button, Form, FormControl, Nav, NavDropdown} from "react-bootstrap";
@@ -12,19 +12,14 @@ function App() {
     <div className="App">
       <Router history={history}>
         <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">Johnson's Group Inc.</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
+              <Nav.Link href="#home">Dashboard</Nav.Link>
+              <Nav.Link href="#link">Data Sources</Nav.Link>
+              <Nav.Link href="#link">Data Analyst</Nav.Link>
+              <Nav.Link href="#link">Data Scientist</Nav.Link>
             </Nav>
             <Form inline>
               <FormControl type="text" placeholder="Search" className="mr-sm-2" />
