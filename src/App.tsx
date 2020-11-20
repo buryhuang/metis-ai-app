cf243144599684f272678d9ca9442c4671a87283
Bury Huang <bury.huang@highspot.com>
2020-11-20 00:52:25 -0800

    add basic connector layout and modal dialog

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,31 @@ import Navbar from 'react-bootstrap/Navbar'
 import {Button, Form, FormControl, Nav, NavDropdown} from "react-bootstrap";
 import { Router, Route } from "react-router-dom";
 import { createBrowserHistory as createHistory } from "history";
+import {DataSources} from "./components/datasources/DataSources";
 const history = createHistory();
 
 function App() {
   return (
     <div className="App">
       <Router history={history}>
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="white" expand="lg">
           <Navbar.Brand href="#home">Johnson's Group Inc.</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
               <Nav.Link href="#home">Dashboard</Nav.Link>
               <Nav.Link href="#sources">Data Sources</Nav.Link>
-              <Nav.Link href="#analyst">Data Analyst</Nav.Link>
               <Nav.Link href="#scientist">Data Scientist</Nav.Link>
             </Nav>
             <Form inline>
               <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-              <Button variant="outline-success">Search</Button>
+              <Button variant="outline-success"><i className="fas fa-search" /></Button>
             </Form>
           </Navbar.Collapse>
         </Navbar>
+        <div className="main-area">
+          <DataSources />
+        </div>
       </Router>
     </div>
   );
---
+++ src/components/datasources/DataSources.tsx
@@ -0,0 +1,110 @@
+import React from 'react';
+
+import './DataSources.css';
+import {Button, Form, Modal} from "react-bootstrap";
+import {Link} from "react-router-dom";
+import {ProviderCsv} from "./providers/ProviderCsv";
+
+interface DataSourcesProps {}
+
+interface DataSourcesState {
+    email: string;
+    code: string;
+    errorMessage: string;
+    showModal: boolean;
+}
+
+class DataSources extends React.Component<
+    DataSourcesProps,
+    DataSourcesState
+    > {
+    state: DataSourcesState;
+
+    constructor(props: DataSourcesProps) {
+        super(props);
+        this.state = {
+            email: '-',
+            code: '',
+            errorMessage: '',
+            showModal: false
+        };
+    }
+
+    async componentDidMount() {
+        // let user = await Auth.currentAuthenticatedUser();
+        // this.setState({ email: user.attributes.email });
+    }
+
+    // handleSuccess = (data) => {
+    //     this.setState({
+    //         code: data.code,
+    //         errorMessage: '',
+    //     });
+    // };
+    //
+    // handleFailure = (error) => {
+    //     this.setState({
+    //         code: '',
+    //         errorMessage: error.errorMessage,
+    //     });
+    // };
+
+    render() {
+        const { code, errorMessage } = this.state;
+
+        return (
+            <div className="main-area">
+                <div className="row">
+                    <div className="col-sm">
+                    </div>
+                    <div className="col-sm">
+                        <input className="theme-input" placeholder="Search for a data source"/><Button variant="outline-success"><i className="fas fa-search" /></Button>
+                    </div>
+                    <div className="col-sm">
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm"></div>
+                    <div className="col-sm"><a>Popular</a></div>
+                    <div className="col-sm"><a>Analytics</a></div>
+                    <div className="col-sm"><a>Social Media</a></div>
+                    <div className="col-sm"><a>Marketing</a></div>
+                    <div className="col-sm"><a>Database</a></div>
+                    <div className="col-sm"></div>
+                </div>
+                <div className="row">
+                    <hr  style={{
+                        color: '#000000',
+                        backgroundColor: '#000000',
+                        height: .5,
+                        borderColor : '#000000'
+                    }}/>
+                </div>
+                <div className="row">
+                    <div className="col-sm" />
+                    <div className="col-sm">
+                        <div className="datasources-box">
+                            <p><img className="datasources-img" src="https://images.ctfassets.net/3n0fku9d0jjr/1caC38kuhK8WusWKQGIeAO/6a35483a56960b0ff258e5c6acb9c0d7/csv-autofetch.svg" /></p>
+                            {/*<a >Link Account</a>*/}
+                            <a onClick={() => this.setState({showModal: true})}>Link Account</a>
+                            {this.state.showModal && <Modal show={this.state.showModal} onHide={() => this.setState({showModal: false})}> <ProviderCsv /> </Modal>}
+                        </div>
+                    </div>
+                    <div className="col-sm">
+                        <div className="datasources-box">
+                        </div>
+                    </div>
+                    <div className="col-sm">
+                        <div className="datasources-box">
+                        </div>
+                    </div>
+                    <div className="col-sm" />
+                    <div className="col-sm" />
+                    <div className="col-sm" />
+                </div>
+            </div>
+        );
+    }
+}
+
+export { DataSources };
