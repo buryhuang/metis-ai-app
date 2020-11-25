f4f5fcb3ec36a4c9e3e76ccaba2d410920670f8a
Bury Huang <bury.huang@highspot.com>
2020-11-25 01:18:57 -0800

    fix datascientiest tabs and run sql query input via api

--- src/components/datascientist/DataScientist.tsx
+++ src/components/datascientist/DataScientist.tsx
@@ -2,11 +2,8 @@ import React, {SyntheticEvent} from 'react';
 
 import './DataScientist.css';
 import Grid from '@material-ui/core/Grid';
+import {SqlQuery} from "./panels/SqlQuery";
 import {Sidebar} from "../shared/sidebar/Sidebar";
-import {Dashboard} from "../dashboard/Dashboard";
-import {DataSources} from "../datasources/DataSources";
-import {Button} from "@material-ui/core";
-import {DataGrid} from "@material-ui/data-grid";
 
 interface DataScientistProps {}
 
@@ -16,9 +13,6 @@ interface DataScientistState {
     errorMessage: string;
     showModal: boolean;
     currentHeaderMenu: number;
-    resultColumns: any;
-    resultRows: any;
-    queryString: string;
 }
 
 class DataScientist extends React.Component<
@@ -35,15 +29,10 @@ class DataScientist extends React.Component<
             errorMessage: '',
             showModal: false,
             currentHeaderMenu: 0,
-            resultColumns: [],
-            resultRows: [],
-            queryString: "SELECT * FROM datastore.csv_sales_table1 LIMIT 100"
         };
         this.handleMenuClick = this.handleMenuClick.bind(this);
         this.renderContentFrame = this.renderContentFrame.bind(this);
         this.getActiveStyle = this.getActiveStyle.bind(this);
-        this.handleRunQuery = this.handleRunQuery.bind(this);
-        this.handleQueryChange = this.handleQueryChange.bind(this);
     }
 
     async componentDidMount() {
@@ -54,36 +43,7 @@ class DataScientist extends React.Component<
     handleMenuClick(value: number): void {
         this.setState({
             currentHeaderMenu: value,
-            resultRows: {}
         });
-    }
-
-    handleQueryChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
-        console.log(event?.target?.value);
-        this.setState({ queryString: event?.target?.value });
-    }
-
-    handleRunQuery(): void {
-        fetch("https://qvn38s6a8h.execute-api.us-east-1.amazonaws.com/dev", {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json; charset=utf-8",
-            }
-        })
-            .then((resp) => {
-                return resp.json()
-            })
-            .then((data) => {
-                const data_obj = JSON.parse(data.body)
-                this.setState({
-                    resultColumns: data_obj['columns'].map((x: any) => {return {'field': x['name'], 'headerName': x['name'], 'width': 60}}),
-                    resultRows: data_obj['rows'],
-                })
-            })
-            .catch((error) => {
-                console.log(error, "Failed to load table data");
-                this.setState({errorMessage: error});
-            })
     }
 
     getActiveStyle(currentMenu: number): string {
@@ -97,11 +57,11 @@ class DataScientist extends React.Component<
         const { currentHeaderMenu } = this.state;
         switch (currentHeaderMenu) {
             case 0:
-                return <Dashboard />;
-            case 1:
-                return <DataSources />;
-            case 2:
-                return <DataScientist />;
+                return <SqlQuery />;
+            // case 1:
+            //     return <DataSources />;
+            // case 2:
+            //     return <DataScientist />;
         }
     }
 
@@ -127,16 +87,7 @@ class DataScientist extends React.Component<
                                     this.handleMenuClick(2)
                                 }>R ANALYSIS</a>
                             </Grid>
-                            <Grid container>
-                                <div className="theme-description-box">
-                                    <p>Metis A.I. Select supports only the SELECT SQL command. Using the Data Analyst console, you can extract up to 40 MB of records from an object that is up to 128 MB in size. To work with larger files or more records, for more complex SQL queries, use Data Science console.</p>
-                                    <Button className="theme-query-button" onClick={this.handleRunQuery}>RUN Query</Button>
-                                </div>
-                                <textarea onChange={this.handleQueryChange} className="theme-query-input-box" placeholder="Search for a data source" value={this.state.queryString}/>
-                                <div className="theme-query-result" >
-                                    <DataGrid rows={this.state.resultRows} columns={this.state.resultColumns} pageSize={10} />
-                                </div>
-                            </Grid>
+                            { this.renderContentFrame() }
                         </Grid>
                         <Grid item xs>
                         </Grid>
---
+++ src/components/datascientist/panels/SqlQuery.tsx
@@ -0,0 +1,87 @@
+import React, {SyntheticEvent} from 'react';
+
+// import './DataScientist.css';
+import Grid from '@material-ui/core/Grid';
+import {Button} from "@material-ui/core";
+import {DataGrid} from "@material-ui/data-grid";
+
+interface SqlQueryProps {
+}
+
+interface SqlQueryState {
+    resultColumns: any;
+    resultRows: any;
+    queryString: string;
+    tableName: string;
+    errorMessage: string;
+}
+
+class SqlQuery extends React.Component<
+    SqlQueryProps,
+    SqlQueryState
+    > {
+    state: SqlQueryState;
+
+    constructor(props: SqlQueryProps) {
+        super(props);
+        this.state = {
+            resultColumns: [],
+            resultRows: [],
+            queryString: "SELECT * FROM datastore.csv_sales_table1 LIMIT 15",
+            tableName: "csv_sales_table1",
+            errorMessage: ""
+        };
+        this.handleRunQuery = this.handleRunQuery.bind(this);
+        this.handleQueryChange = this.handleQueryChange.bind(this);
+    }
+
+    async componentDidMount() {
+        // let user = await Auth.currentAuthenticatedUser();
+        // this.setState({ email: user.attributes.email });
+    }
+
+    handleQueryChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
+        this.setState({ queryString: event?.target?.value });
+    }
+
+    handleRunQuery(): void {
+        fetch("https://cs2vycpq5j.execute-api.us-east-1.amazonaws.com/dev?query=" + encodeURI(this.state.queryString) + "&table=" + encodeURI(this.state.tableName), {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+            }
+        })
+            .then((resp) => {
+                return resp.json()
+            })
+            .then((data_obj) => {
+                this.setState({
+                    resultColumns: data_obj['columns'].map((x: any) => {return {'field': x['name'], 'headerName': x['name'], 'width': 60}}),
+                    resultRows: data_obj['rows'],
+                })
+            })
+            .catch((error) => {
+                console.log(error, "Failed to load table data");
+                this.setState({errorMessage: error});
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                <Grid container>
+                    <div className="theme-description-box">
+                        <p>Metis A.I. Select supports only the SELECT SQL command. Using the Data Analyst console, you can extract up to 40 MB of records from an object that is up to 128 MB in size. To work with larger files or more records, for more complex SQL queries, use Data Science console.</p>
+                        <Button className="theme-query-button" onClick={this.handleRunQuery}>RUN Query</Button>
+                    </div>
+                    <textarea onChange={this.handleQueryChange} className="theme-query-input-box" placeholder="Search for a data source" value={this.state.queryString}/>
+                    <div className="theme-query-result" >
+                        <DataGrid rows={this.state.resultRows} columns={this.state.resultColumns} pageSize={10} />
+                    </div>
+                </Grid>
+            </div>
+        );
+    }
+}
+
+export { SqlQuery };
